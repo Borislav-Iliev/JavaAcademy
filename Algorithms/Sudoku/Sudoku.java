1124212e64b2eda8bd3e915f367aebfb6d9ec0d4
Borislav Iliev <borislav.iliev2002@gmail.com>
2023-07-13 14:19:27 +0300

    Sudoku solver

---
+++ Algorithms/Sudoku/Sudoku.java
@@ -0,0 +1,91 @@
+public class Sudoku {
+    public static void main(String[] args) {
+        int[][] board = {
+                {7, 0, 2, 0, 5, 0, 6, 0, 0},
+                {0, 0, 0, 0, 0, 3, 0, 0, 0},
+                {1, 0, 0, 0, 0, 9, 5, 0, 0},
+                {8, 0, 0, 0, 0, 0, 0, 9, 0},
+                {0, 4, 3, 0, 0, 0, 7, 5, 0},
+                {0, 9, 0, 0, 0, 0, 0, 0, 8},
+                {0, 0, 9, 7, 0, 0, 0, 0, 5},
+                {0, 0, 0, 2, 0, 0, 0, 0, 0},
+                {0, 0, 7, 0, 4, 0, 2, 0, 3}
+        };
+
+        if (solveBoard(board)) {
+            System.out.println("Successfully solved the sudoku board:");
+            printBoard(board);
+        } else {
+            System.out.println("Board is impossible to be solved!");
+        }
+    }
+
+    private static void printBoard(int[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int column = 0; column < board[row].length; column++) {
+                System.out.print(board[row][column] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean isNumberInRow(int[][] board, int number, int row) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumberInColumn(int[][] board, int number, int column) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
+        int currentRow = row - row % 3;
+        int currentColumn = column - column % 3;
+
+        for (int i = currentRow; i < currentRow + 3; i++) {
+            for (int j = currentColumn; j < currentColumn + 3; j++) {
+                if (board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
+        return !isNumberInRow(board, number, row) &&
+                !isNumberInColumn(board, number, column) &&
+                !isNumberInBox(board, number, row, column);
+    }
+
+    private static boolean solveBoard(int[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int column = 0; column < board[row].length; column++) {
+                if (board[row][column] == 0) {
+                    for (int i = 1; i <= 9; i++) {
+                        if (isValidPlacement(board, i, row, column)) {
+                            board[row][column] = i;
+
+                            if (solveBoard(board)) {
+                                return true;
+                            } else {
+                                board[row][column] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
