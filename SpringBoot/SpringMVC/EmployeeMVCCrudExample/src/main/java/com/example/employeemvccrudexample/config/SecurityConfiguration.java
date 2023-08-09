903d0d7350601679b10231341da1487ae418b919
Borislav Iliev <borislav.iliev2002@gmail.com>
2023-08-09 11:30:16 +0300

    Added login and logout functionality

--- SpringBoot/SpringMVC/EmployeeMVCCrudExample/src/main/java/com/example/employeemvccrudexample/config/SecurityConfiguration.java
+++ SpringBoot/SpringMVC/EmployeeMVCCrudExample/src/main/java/com/example/employeemvccrudexample/config/SecurityConfiguration.java
@@ -3,14 +3,17 @@ package com.example.employeemvccrudexample.config;
 import com.example.employeemvccrudexample.model.entity.Employee;
 import com.example.employeemvccrudexample.repository.EmployeeRepository;
 import com.example.employeemvccrudexample.service.EmployeeUserDetailsService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfiguration {
@@ -23,8 +26,24 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults());
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/auth/login", "/auth/register").permitAll()
+                        .requestMatchers("/employees/add", "/employees/update/**").hasRole("MANAGER")
+                        .requestMatchers("/employees/delete/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login
+                        .loginPage("/auth/login")
+                        .defaultSuccessUrl("/employees")
+                        .failureForwardUrl("/auth/login-error")
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/auth/logout")
+                        .logoutSuccessUrl("/auth/login")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                );
 
         return http.build();
     }
--- SpringBoot/SpringMVC/EmployeeMVCCrudExample/src/main/resources/data.sql
+++ SpringBoot/SpringMVC/EmployeeMVCCrudExample/src/main/resources/data.sql
@@ -1,4 +1,5 @@
-
-
 INSERT INTO `role`
 VALUES (1, "EMPLOYEE"), (2, "MANAGER"), (3, "ADMIN");
+
+INSERT INTO `employee_role`
+VALUES (2, 2), (3, 2), (3, 3);
